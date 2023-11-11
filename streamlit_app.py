7c3351945ab36f36b244598e1cf70ac165d233fb
Igecchi <57622015+Igecchi@users.noreply.github.com>
2023-11-11 14:14:09 +0900

    test st.slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import altair as alt
 import numpy as np
 from google.oauth2 import service_account
 from google.cloud import bigquery
+from datetime import time, datetime
 
 st.title('Connect Streamlit to Google Bigquery')
 
@@ -37,32 +38,39 @@ query ="""
 #     st.write("✍️ " + row['word'])
 # st.balloons()
 
-st.header("Test st.write()")
+st.header("Test st.slider()")
 
 # ex.1
-st.write('Hello, *World!* :sunglasses:')
+st.subheader("Slider")
+
+age = st.slider("How old are you?", 0, 130, 33)
+st.write("I'm ", age, "years old")
 
 # ex.2
-st.write(1234)
+st.subheader("Range Slider")
+
+values = st.slider(
+    "select a range of values",
+    0.0, 100.0, (25.0, 75.0)
+    )
+st.write("Valus:", values)
 
 # ex.3
-st.write('1 + 1 = ', 2)
+st.subheader("Range time slider")
+
+appointment = st.slider(
+    "Schedule your appointment:",
+    value = (time(11, 30), time(12, 45))
+    )
+st.write("You're schedule for:", appointment)
 
 # ex.4
-df = pd.DataFrame({
-    'first column':  [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
-st.write(df)
+st.subheader("Datetime slider")
 
-# ex.5
-st.write('Below is a Dataframe:', df, 'Above is a Dataframe')
-
-# ex.6
-df2 = pd.DataFrame(
-    np.random.randn(20, 3)
-    , columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(df2)
-st.write(c)
+start_time = st.slider(
+    "When do you start?",
+    value = datetime(2020, 1, 1, 9, 30),
+    format = "YYYY/MM/DD - hh:mm"
+    # format = "MM/DD/YY - hh:mm"
+    )
+st.write("Start time:", start_time)
